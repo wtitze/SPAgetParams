2aba4e48e94f331181612e387aac15e5c74cd14c
wtitze <w.titze@gmail.com>
2024-11-12 15:14:31 +0000

    passaggio di parametri nell'URL

--- serverAPI/app.py
+++ serverAPI/app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 CORS(app)
 
 # Route to handle the sum calculation using GET
-@app.route('/sum', methods=['GET'])
-def calculate_sum():
-    num1 = request.args.get('num1', 0)  # Retrieve num1 from query parameters, default to 0
-    num2 = request.args.get('num2', 0)  # Retrieve num2 from query parameters, default to 0
+@app.route('/sum/<num1>/<num2>', methods=['GET'])
+def calculate_sum(num1, num2):
     try:
         result = float(num1) + float(num2)
     except ValueError:
